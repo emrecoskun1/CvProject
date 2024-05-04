3b07a526d5f73fc7365aa9897cf9d8013b0a5e97
Yunus Emre Coskun <emre27coskun@gmail.com>
2024-05-04 23:46:54 +0300

    fluent validation packages were added and configurations were made

--- Controllers/PortfolioController.cs
+++ Controllers/PortfolioController.cs
@@ -1,7 +1,10 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Update;
 
 namespace Project.Controllers
 {
@@ -19,16 +22,69 @@ namespace Project.Controllers
         [HttpGet]
         public IActionResult AddPortfolio()
         {
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Projelerim";
+            ViewBag.v3 = "Proje Ekleme";
             return View();
         }
         [HttpPost]
         public IActionResult AddPortfolio(Portfolio p)
         {
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult results = validations.Validate(p);
+            if(results.IsValid)
+            {
+                portfolioManager.TAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                    
+                }
+            }
+            return View();
+        }
+        public IActionResult DeletePortfolio(int id)
+        {
+            var values = portfolioManager.TGetById(id);
+            portfolioManager.TDelete(values);
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult EditPortfolio(int id)
+        {
             ViewBag.v1 = "Proje Listesi";
             ViewBag.v2 = "Projelerim";
-            ViewBag.v3 = "Proje Ekleme";
-            portfolioManager.TAdd(p);   
-            return RedirectToAction("Index");
+            ViewBag.v3 = "Proje Düzenleme";
+            var values = portfolioManager.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditPortfolio(Portfolio portfolio)
+        {
+            {
+                PortfolioValidator validations = new PortfolioValidator();
+                ValidationResult results = validations.Validate(portfolio);
+                if(results.IsValid)
+                {
+                    portfolioManager.TUpdate(portfolio);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var item in results.Errors )
+                    {
+                        ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                        
+                    }
+                }
+                return View();
+            }
         }
     }
 }
